46fc298e70c57b3b0ac2db9f614c08d254f8b287
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-04-10 19:53:01 +0530

    Create colorcode.h

---
+++ colorcode.h
@@ -0,0 +1,26 @@
+ifndef _COLORCODE_H
+#define _COLORCODE_H
+
+#define MAX_COLORPAIR_NAME_CHARS 16
+
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair*, char*);
+ColorPair GetColorFromPairNumber(int);
+int GetPairNumberFromColor(const ColorPair*);
+void testNumberToPair(int , enum MajorColor,enum MinorColor);
+void testPairToNumber(enum MajorColor, enum MinorColor, int);
+
+#endif
