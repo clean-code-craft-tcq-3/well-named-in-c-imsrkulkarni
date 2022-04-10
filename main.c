5884faf9667f4b5f2ee040409182c3a46bdefaa3
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-04-10 19:49:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
+
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
