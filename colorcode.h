ca7609b8727ca35899ffb785e5fc7f223d1414e3
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-04-12 12:11:09 +0530

    Update colorcode.h

--- colorcode.h
+++ colorcode.h
@@ -22,5 +22,6 @@ ColorPair GetColorFromPairNumber(int);
 int GetPairNumberFromColor(const ColorPair*);
 void testNumberToPair(int , enum MajorColor,enum MinorColor);
 void testPairToNumber(enum MajorColor, enum MinorColor, int);
+void printReferenceManual();
 
 #endif
