b28570dcf9b5755e3f63808b1ab3fde69b99a0e6
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-04-13 11:14:16 +0530

    Update ColorCode_ReferenceMan.c

--- ColorCode_ReferenceMan.c
+++ ColorCode_ReferenceMan.c
@@ -4,13 +4,19 @@
 
 void printReferenceManual() 
 {
-   int referManIndex=1,majorColorIndex,minorColorIndex;
+   int referManIndex=1,majorColorIndex;
    printf("PairNumber\t  MajorColor\t     MinorColor\n");
    for(majorColorIndex=0;majorColorIndex<numberOfMajorColors;majorColorIndex++)
    {
-      for(minorColorIndex=0;minorColorIndex<numberOfMinorColors;minorColorIndex++)
-      {
-          printf("%d\t %s\t  %s\n",referManIndex++,MajorColorNames[majorColorIndex],MinorColorNames[minorColorIndex]);
-      }
+     mapMinorColorWithMajorColor(MajorColorNames[majorColorIndex])
    }
  }
+
+void mapMinorColorWithMajorColor(char* majorColor)
+{
+   int minorColorIndex;
+   for(minorColorIndex=0;minorColorIndex<numberOfMinorColors;minorColorIndex++)
+      {
+          printf("%d\t %s\t  %s\n",referManIndex++,majorColor,MinorColorNames[minorColorIndex]);
+      }
+}
